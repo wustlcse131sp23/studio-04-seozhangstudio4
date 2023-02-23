35e0aba0627458bbe4f71354eb4271acd49d3de1
averyseo <averyseo@localhost>
2023-02-23 15:24:41 -0600

    Studio 4- Avery and Junwei

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,24 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(StdDraw.GRAY);
+		StdDraw.filledSquare(1, 1, 1);
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledRectangle(.5, .5, .3, .2); //10 points for a 5 point star
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledEllipse(.3,.6,.025,.025);
+		StdDraw.filledEllipse(.4, .65, .015, .015);
+		StdDraw.filledEllipse(.38, .57, .015, .015);
+		StdDraw.filledEllipse(.34, .49, .015, .015);
 	}
 }
